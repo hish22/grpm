683c5796bcf5ddd1a22ff05a788677330cdebd48
hisham jog <hish522@hotmail.com>
2026-01-27 20:09:24 +0300

    feat: add open TOML file flag

--- cmd/configc/configCommand.go
+++ cmd/configc/configCommand.go
@@ -8,7 +8,10 @@ import (
 )
 
 var (
-	show bool
+	show     bool // Print TOML config information
+	rfconfig bool // Allow refactor
+	//rconfig   string // Refactor config type (like location)
+	//crname    string // Refactor value of the config type (like location = "/usr/bin")
 )
 
 func Config() *cobra.Command {
@@ -18,14 +21,16 @@ func Config() *cobra.Command {
 		Run:   ConfigCmd,
 	}
 	c.Flags().BoolVarP(&show, "show", "s", false, "Show TOML configuration information")
-
+	c.Flags().BoolVarP(&rfconfig, "open", "o", false, "Open TOML configuration file")
 	return c
 }
 
 func ConfigCmd(cmd *cobra.Command, args []string) {
+	c := config.DecodeTOMLConfig()
 	if show {
-		c := config.DecodeTOMLConfig()
 		fmt.Println("==========grpm Configuration==========")
 		fmt.Println("Location:", "\033[1m", c.Location, "\033[0m", "=> Where your installed files are saved")
+	} else if rfconfig {
+		config.OpenTOMLConfig()
 	}
 }
