8c57b544cfcd904cad8e88b7f76ba1c2f35341ba
hisham jog <hish522@hotmail.com>
2026-01-27 20:08:22 +0300

    refactor: separate UserHomeDir into a function.

--- internal/config/generate.go
+++ internal/config/generate.go
@@ -7,26 +7,34 @@ import (
 	"path/filepath"
 )
 
-func GenerateTOMLConfig() {
-
+func homeDir() string {
 	homep, err := os.UserHomeDir()
 	if err != nil {
 		log.Fatal("Can't detect home dir, ", err)
 	}
+	return homep
+}
 
-	localConfigDirPath := filepath.Join(homep, ".config", "grpm")
-	localConfigDirToml := filepath.Join(homep, ".config", "grpm", "config.toml")
+func LocalConfigDirPath() string {
+	return filepath.Join(homeDir(), ".config", "grpm")
+}
+
+func LocalConfigDirToml() string {
+	return filepath.Join(homeDir(), ".config", "grpm", "config.toml")
+}
+
+func GenerateTOMLConfig() {
 
 	payload := []byte(`location = "/usr/local/bin"`)
 
-	if err := os.MkdirAll(localConfigDirPath, 0755); err != nil {
+	if err := os.MkdirAll(LocalConfigDirPath(), 0755); err != nil {
 		log.Fatal("Can't create grpm dir, ", err)
 	}
 
-	if err := os.WriteFile(localConfigDirToml, payload, 0644); err != nil {
+	if err := os.WriteFile(LocalConfigDirToml(), payload, 0644); err != nil {
 		log.Fatal("Can't create config.toml, ", err)
 	}
 
-	fmt.Printf("Config created at: %s\n", localConfigDirToml)
+	fmt.Printf("Config created at: %s\n", LocalConfigDirToml())
 
 }
