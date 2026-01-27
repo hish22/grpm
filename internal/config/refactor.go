e125f96347ba2304f8c3efb7b5f0fd1a5c88067b
hisham jog <hish522@hotmail.com>
2026-01-27 20:12:06 +0300

    feat: allow user to open a nano:linux or windows:notepad to edit TOML config file

---
+++ internal/config/refactor.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"runtime"
+)
+
+func OpenTOMLConfig() {
+	switch runtime.GOOS {
+	case "linux":
+		cmd := exec.Command("nano", LocalConfigDirToml())
+
+		// Connect the command's input/output to the current terminal
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		if err := cmd.Run(); err != nil {
+			log.Fatal("Can't open TOML config file, ", err)
+		}
+	case "windows":
+		cmd := exec.Command("notepad", LocalConfigDirToml())
+		if err := cmd.Run(); err != nil {
+			log.Fatal("Can't open TOML config file, ", err)
+		}
+	}
+}
+
+// func RefactorConfigs(uconfig *config) {
+
+// 	buf := new(bytes.Buffer)
+
+// 	if err := toml.NewEncoder(buf).Encode(uconfig); err != nil {
+// 		log.Fatal("Can't Encode config, ", err)
+// 	}
+
+// 	fmt.Println(buf)
+
+// }
