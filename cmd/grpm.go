0898c764c615e5f371f7c729a787df573e1369c9
hisham jog <hish522@hotmail.com>
2026-01-27 18:32:25 +0300

    feat: add --define flag to init a config TOML file.

--- cmd/grpm.go
+++ cmd/grpm.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-	"hish22/grpm/cmd/search"
+	"hish22/grpm/cmd/configc"
+	"hish22/grpm/cmd/searchc"
+	"hish22/grpm/internal/config"
 
 	"github.com/spf13/cobra"
+)
+
+var (
+	define bool
 )
 
 func root() *cobra.Command {
@@ -11,16 +17,22 @@ func root() *cobra.Command {
 		Use:   "grpm",
 		Short: "A cool github release packet manger",
 		Long:  `Github Releases Packet Manager (grpm) is a tool to handle installed releases from github.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			if define {
+				config.GenerateTOMLConfig()
+			} else {
+				cmd.Help()
+			}
+		},
 	}
 }
 
 func main() {
-
 	r := root()
-
+	r.Flags().BoolVarP(&define, "define", "d", false, "initialize your grpm tool")
 	// Add search command
-	r.AddCommand(search.Search())
-
+	r.AddCommand(searchc.Search())
+	r.AddCommand(configc.Config())
 	if err := r.Execute(); err != nil {
 		panic(err)
 	}
