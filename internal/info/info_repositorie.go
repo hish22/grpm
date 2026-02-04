a1a5501df552f385a1d5fb9187bb7d6ca787d647
hisham jog <hish522@hotmail.com>
2026-02-04 23:17:02 +0300

    refactor: change info logic

--- internal/info/info_repositorie.go
+++ internal/info/info_repositorie.go
@@ -4,6 +4,7 @@ import (
 	corehttp "hish22/grpm/internal/coreHttp"
 	"hish22/grpm/internal/persistance"
 	"hish22/grpm/internal/serialization"
+	"hish22/grpm/internal/structures"
 	"io"
 	"log"
 )
@@ -52,6 +53,19 @@ func convertIntoInfoRepo(response *Response, link *string) RepoPageInfo {
 		CreatedAt: response.Payload.Repo.CreatedAt,
 		Readme:    response.Payload.Tree.Readme.RichText,
 	}
+}
+
+func InfoRepository(owner *string, name *string) *structures.Repository {
+	var repository structures.Repository
+	link := corehttp.RequestLink{
+		Base:      corehttp.ApiLink,
+		Endpoints: []string{"repos", *owner, *name},
+	}.Build()
+	if !persistance.FetchFromCache(&repository, *link) {
+		corehttp.Request(link, &repository)
+		persistance.NewCache(*link, &repository)
+	}
+	return &repository
 }
 
 func JsonInfoRepo(owner *string, repo *string) (RepoPageInfo, error) {
