47835cacc2186b4c3fc886f17aa6837738491b53
hisham jog <hish522@hotmail.com>
2026-02-07 16:17:56 +0300

    refactor: return error from FetchAssetsWithoutPrint() to handle 0 tracked

--- internal/list/list_assets.go
+++ internal/list/list_assets.go
@@ -4,13 +4,21 @@ import (
 	"fmt"
 	"hish22/grpm/internal/asset"
 
+	charmlog "github.com/charmbracelet/log"
 	"github.com/dustin/go-humanize"
 )
 
 func ListAssets() {
-	ta := asset.FetchAssetsWithoutPrint()
+	trackedAssets, err := asset.FetchAssetsWithoutPrint()
+	if err != nil {
+		if err.Error() == "no such table: asset" {
+			charmlog.Fatal("No installed assets to list,", "Error", err)
+		} else {
+			charmlog.Fatal(err)
+		}
+	}
 	fmt.Println("=== Installed Assets (Tracked) ===")
-	for _, a := range ta {
+	for _, a := range trackedAssets {
 		fmt.Println(a.ID, a.AssetName, a.Tag, "("+humanize.Bytes(uint64(a.Size))+")")
 	}
 }
