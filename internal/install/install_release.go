e6000c13bbdf227557dd4ca4d537ab9f9bdc9351
hisham jog <hish522@hotmail.com>
2026-02-09 14:04:10 +0300

    refactor: migrate track process logic into track_asset.go

--- internal/install/install_release.go
+++ internal/install/install_release.go
@@ -5,7 +5,6 @@ import (
 	"encoding/hex"
 	"fmt"
 	"hish22/grpm/internal/config"
-	"hish22/grpm/internal/persistance"
 	"hish22/grpm/internal/structures"
 	"io"
 	"net/http"
@@ -93,23 +92,4 @@ func InstallSelectedAsset(repo *string, asset *structures.Assets, release *struc
 	trackAssetTable()                   // Create the table if not exists
 	registerAsset(repo, asset, release) // Register installed asset
 
-}
-
-func trackAssetTable() {
-	db := persistance.OpenMetadataDB()
-	_, err := db.Exec("CREATE TABLE IF NOT EXISTS asset (id INT PRIMARY KEY, repo TEXT,asset_name TEXT, location TEXT, tag TEXT, release_name TEXT, size INT, Digest TEXT);")
-	if err != nil {
-		charmlog.Fatal("Can't create asset table to track assets, ", err)
-	}
-}
-
-func registerAsset(repo *string, asset *structures.Assets, release *structures.Release) {
-	db := persistance.OpenMetadataDB()
-	path := writePath(&asset.AssetName)
-	_, err := db.Exec("INSERT INTO asset VALUES (?,?,?,?,?,?,?,?);", asset.ID, *repo, asset.AssetName, path, release.TagName, release.ReleaseName, asset.Size, asset.Digest)
-	if err != nil {
-		charmlog.Warn("Failed to register an installed asset", "error", err)
-		return
-	}
-	charmlog.Info("Asset registered (Tracked)")
 }
---
+++ internal/install/track_asset.go
@@ -0,0 +1,27 @@
+package install
+
+import (
+	"hish22/grpm/internal/persistance"
+	"hish22/grpm/internal/structures"
+
+	charmlog "github.com/charmbracelet/log"
+)
+
+func trackAssetTable() {
+	db := persistance.OpenMetadataDB()
+	_, err := db.Exec("CREATE TABLE IF NOT EXISTS asset (id INT PRIMARY KEY, repo TEXT,asset_name TEXT, location TEXT, tag TEXT, release_name TEXT, size INT, Digest TEXT);")
+	if err != nil {
+		charmlog.Fatal("Can't create asset table to track assets, ", err)
+	}
+}
+
+func registerAsset(repo *string, asset *structures.Assets, release *structures.Release) {
+	db := persistance.OpenMetadataDB()
+	path := writePath(&asset.AssetName)
+	_, err := db.Exec("INSERT INTO asset VALUES (?,?,?,?,?,?,?,?);", asset.ID, *repo, asset.AssetName, path, release.TagName, release.ReleaseName, asset.Size, asset.Digest)
+	if err != nil {
+		charmlog.Warn("Failed to register an installed asset", "error", err)
+		return
+	}
+	charmlog.Info("Asset registered (Tracked)")
+}
