ffbc6df95a45cb7198da564b79db4296cfa002ac
hisham jog <hish522@hotmail.com>
2026-02-15 16:07:04 +0300

    refactor: handle home path when sudo command used with grpm

--- internal/util/dirs.go
+++ internal/util/dirs.go
@@ -1,14 +1,31 @@
 package util
 
 import (
-	"log"
+	"fmt"
 	"os"
+	"path/filepath"
+	"runtime"
 )
 
-func HomeDir() string {
-	homep, err := os.UserHomeDir()
-	if err != nil {
-		log.Fatal("Can't detect home dir, ", err)
+func HomeDir() (string, error) {
+	switch runtime.GOOS {
+	case "linux":
+		if os.Geteuid() == 0 {
+			return filepath.Join("/", "home", os.Getenv("SUDO_USER")), nil
+		} else {
+			home, err := os.UserHomeDir()
+			if err != nil {
+				return "", err
+			}
+			return home, nil
+		}
+	case "windows":
+		home, err := os.UserHomeDir()
+		if err != nil {
+			return "", err
+		}
+		return home, nil
+	default:
+		return "", fmt.Errorf("Failed to fetch home path")
 	}
-	return homep
 }
