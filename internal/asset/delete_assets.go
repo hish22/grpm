3c399c4914bbdb1cc181b3f39a14851e2a8be161
hisham jog <hish522@hotmail.com>
2026-02-16 22:28:08 +0300

    feat: add delete raw asset and setup asset logic

--- internal/asset/delete_assets.go
+++ internal/asset/delete_assets.go
@@ -2,6 +2,7 @@ package asset
 
 import (
 	"hish22/grpm/internal/persistance"
+	"os"
 
 	charmlog "github.com/charmbracelet/log"
 )
@@ -11,5 +12,22 @@ func DeleteLastTrackedAssetById(id int) {
 	_, err := db.Exec("DELETE FROM asset WHERE id=?", id)
 	if err != nil {
 		charmlog.Error("Failed to delete last tracked asset", "error", err)
+	}
+}
+
+func RemoveRawAsset(assetLocation string) {
+	err := os.RemoveAll(assetLocation)
+	if err != nil {
+		charmlog.Error("Failed to delete old raw asset folder", "error", err)
+	}
+}
+
+func RemoveAssetLibFile(assetID int) {
+	assetLibLocation := FileSetupLocation(assetID)
+	if assetLibLocation != "" {
+		err := os.RemoveAll(assetLibLocation)
+		if err != nil {
+			charmlog.Error("Failed to delete old asset folder", "error", err)
+		}
 	}
 }
