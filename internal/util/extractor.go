738097867736a7d7139d84a0c8031dd74c3e7965
hish22 <hish522@hotmail.com>
2026-02-18 13:57:49 +0300

    feat: add name and extension extractor

--- internal/util/extractor.go
+++ internal/util/extractor.go
@@ -15,9 +15,18 @@ func removeDot(extensions []string) {
 func ExtensionExtractor(file string) []string {
 	regx, err := regexp.Compile(`\.\D\w*`)
 	if err != nil {
-		charmlog.Fatal("Failed to compile extractor regular expression", "error", err)
+		charmlog.Error("Failed to compile extractor regular expression", "error", err)
 	}
 	extensions := regx.FindAllString(file, -1)
 	removeDot(extensions)
 	return extensions
+}
+
+func NameAndExtensionExtractor(file string) []string {
+	regx, err := regexp.Compile(`(.+)\.\D\w*`)
+	if err != nil {
+		charmlog.Error("Failed to compile name and ext extractor regular expression", "error", err)
+	}
+	namesAndExtensions := regx.FindStringSubmatch(file)
+	return namesAndExtensions
 }
--- internal/util/extractor_test.go
+++ internal/util/extractor_test.go
@@ -15,3 +15,16 @@ func TestExtensionExtractor(t *testing.T) {
 	}
 
 }
+
+func TestNameAndExtensionExtractor(t *testing.T) {
+	file := "fzf-0.67.0-windows_amd64.zip"
+	result := NameAndExtensionExtractor(file)
+
+	if result[0] != "fzf-0.67.0-windows_amd64.zip" {
+		t.Errorf("Expected fzf-0.67.0-windows_amd64.zip as first extension, got:%s", result[0])
+	}
+
+	if result[1] != "fzf-0.67.0-windows_amd64" {
+		t.Errorf("Expected fzf-0.67.0-windows_amd64 as first extension, got:%s", result[1])
+	}
+}
