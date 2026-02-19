44ceccffbed093ce6be94a56bc784839398eff47
hish22 <hish522@hotmail.com>
2026-02-20 01:57:49 +0300

    refactor: make sure parent dir is there when unzip tar files in linux

--- internal/setup/unzip_tar_linux.go
+++ internal/setup/unzip_tar_linux.go
@@ -12,22 +12,22 @@ import (
 	charmlog "github.com/charmbracelet/log"
 )
 
-func unzipTar(repo string, header *tar.Header, file io.Reader, assetID int) {
+func unzipTar(repo string, header *tar.Header, assetName string, file io.Reader, assetID int) {
 	switch header.Typeflag {
 
 	case tar.TypeDir:
-		err := os.MkdirAll(link.WriteLibFilePath(header.Name), 0755)
+		err := os.MkdirAll(assetName, 0755)
 		if err != nil {
 			charmlog.Fatal("Failed to create directory", "error", err)
 		}
 
 	case tar.TypeReg:
-		asssetlink := link.WriteLibFilePath(header.Name)
+		// asssetlink := link.WriteLibFilePath(header.Name)
 		// Ensure parent directory exists
-		if err := os.MkdirAll(link.WriteLibFilePath(filepath.Dir(header.Name)), 0755); err != nil {
-			charmlog.Warn("Failed to create directory", "error", err)
-		}
-		f, err := os.Create(asssetlink)
+		// if err := os.MkdirAll(link.WriteLibFilePath(filepath.Dir(header.Name)), 0755); err != nil {
+		// 	charmlog.Warn("Failed to create directory", "error", err)
+		// }
+		f, err := os.Create(assetName)
 		if err != nil {
 			charmlog.Fatal("Failed to create file", "error", err)
 		}
@@ -35,8 +35,8 @@ func unzipTar(repo string, header *tar.Header, file io.Reader, assetID int) {
 		if err != nil {
 			charmlog.Fatal("Failed to copy contents of a file", "error", err)
 		}
-		binaryName := strings.Split(header.Name, "/")
-		SymlinkAsset(repo, asssetlink, binaryName[len(binaryName)-1], assetID)
+		binaryName := strings.Split(assetName, "/")
+		SymlinkAsset(repo, assetName, binaryName[len(binaryName)-1], assetID)
 	}
 }
 
@@ -44,7 +44,9 @@ func tarReader(repo string, cfile io.Reader, location string, assetID int) {
 	// read archive .tar file
 	tarfile := tar.NewReader(cfile)
 	charmlog.Info("Extracting..", "asset", location)
-	isSetupFileRegisterd := false
+	// isSetupFileRegisterd := false
+	isThereAParentDir := false
+	parentPath := ""
 	for {
 		header, err := tarfile.Next()
 		if err == io.EOF {
@@ -54,11 +56,26 @@ func tarReader(repo string, cfile io.Reader, location string, assetID int) {
 		if err != nil {
 			charmlog.Error("Failed to read tar", "error", err)
 		}
-		if !isSetupFileRegisterd {
+		// check if first file is parent dir
+		if header.FileInfo().IsDir() && !isThereAParentDir {
+			isThereAParentDir = true
 			asset.InsertFileSetupLocation(link.WriteLibFilePath(header.Name), assetID)
-			isSetupFileRegisterd = true
+			// if not create one
+		} else if !isThereAParentDir {
+			parentPath = link.WriteLibFilePath(repo)
+			if err := os.MkdirAll(parentPath, 0755); err != nil {
+				charmlog.Warn("Failed to create directory", "error", err)
+			}
+			isThereAParentDir = true
+			asset.InsertFileSetupLocation(parentPath, assetID)
 		}
-		charmlog.Info(header.Name, "asset_id", assetID)
-		unzipTar(repo, header, tarfile, assetID)
+		assetName := filepath.Join(parentPath, header.Name)
+		// if !isSetupFileRegisterd && isThereAParentDir {
+
+		// 	isSetupFileRegisterd = true
+		// }
+
+		charmlog.Info(assetName, "asset_id", assetID)
+		unzipTar(repo, header, assetName, tarfile, assetID)
 	}
 }
