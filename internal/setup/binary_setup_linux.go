8fb2a16643b1d6b330780488e8fa116de6b3a14c
hish22 <hish522@hotmail.com>
2026-02-21 00:22:10 +0300

    refactor: pass binaryName instead of repo name in SymlinkAsset

--- internal/setup/binary_setup_linux.go
+++ internal/setup/binary_setup_linux.go
@@ -12,20 +12,19 @@ import (
 
 func MoveBinary(repo string, location string, assetID int, force bool) {
 	if util.IsBinary(location) {
-
 		// Make parent dir
 		parentPath := link.WriteLibFilePath(repo)
 		if err := os.MkdirAll(parentPath, 0755); err != nil {
 			charmlog.Error("Failed to create parent directory", "error", err)
 			return
 		}
-
-		newLink := filepath.Join(parentPath, filepath.Base(location))
+		binaryName := filepath.Base(location)
+		newLink := filepath.Join(parentPath, binaryName)
 		if err := os.Rename(location, newLink); err != nil {
 			charmlog.Error("Failed to move binary from Downloads to lib", "error", err)
 			return
 		}
-		SymlinkAsset(repo, newLink, filepath.Base(location), assetID, force)
+		SymlinkAsset(binaryName, newLink, binaryName, assetID, force)
 		asset.InsertFileSetupLocation(newLink, assetID)
 	}
 }
