3138fc61b3dedb3a461876818b9a101141bf281a
hisham jog <hish522@hotmail.com>
2026-02-21 22:20:44 +0300

    docs: add docs to install_release.go

--- internal/install/install_release.go
+++ internal/install/install_release.go
@@ -41,6 +41,7 @@ import (
 // 3- apply steps
 // 4- traksing could be opitional
 
+/* Hit DB to check if a sepcfifc asset is already installed */
 func isInstalled(repo string) (*structures.TrackedAsset, bool) {
 	asset, err := assets.FetchSpecificAsset(repo)
 	if err != nil {
@@ -49,6 +50,13 @@ func isInstalled(repo string) (*structures.TrackedAsset, bool) {
 	return &asset, true
 }
 
+/*
+	Start The Download operation
+
+Fetch grpm/Downloads path first, then we insert the http response body payload
+into a .tmp file. provide progress bar + hashing to check asset digest.
+if digest check succeed, we start by renaming/creating the fetched asset.
+*/
 func downloadWithValidation(asset *structures.Assets, resp *http.Response) error {
 	// Bring downloads (as tmp file) location
 	downloads, err := config.GrpmDownloadedDirPath()
@@ -86,6 +94,12 @@ func downloadWithValidation(asset *structures.Assets, resp *http.Response) error
 	return os.Rename(tf.Name(), link.WriteDownloadsFilePath(asset.AssetName))
 }
 
+/*
+	Change file permissions
+
+To allow a user to extract and use the download file,
+by changing the file's permission mode into 0644
+*/
 func changeFilePerm(asset string) {
 	err := os.Chmod(link.WriteDownloadsFilePath(asset), 0644)
 	if err != nil {
@@ -93,6 +107,16 @@ func changeFilePerm(asset string) {
 	}
 }
 
+/*
+Install an asset process
+
+Check if user is running this with privileged execution
+then, Check if asset is already installed.
+
+Request to Fetch assets from specific release.
+
+start with setup process only if the user flaged with --setup.
+*/
 func InstallSelectedAsset(repo string, asset *structures.Assets, release *structures.Release, setupStatus bool, force bool) {
 	// Check if user is running this with privileged execution
 	if !util.IsAdministrator() {
